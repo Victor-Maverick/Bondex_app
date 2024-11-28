186b196158f09bc20aeabc9028e5683e8152d7b2
Yisaterese <teresejosephyisa@gmail.com>
2024-11-28 22:58:01 +0100

    added hover effect to the service component

--- bondex/components/homepage/Services.tsx
+++ bondex/components/homepage/Services.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 import { OurServiceProps } from "../../types/Types";
 
-const Services: React.FC<OurServiceProps> = ({ image, text1, text2, text3, text4 }) => {
+const Services: React.FC<OurServiceProps> = ({ image, text1, text2, text3, text4, onClick }) => {
     return (
         <div className={'mb-8 overflow-auto justify-items-center left-0 py-5 text-center'} style={{ backgroundColor: '#09A896' }}>
             <Image src={image} alt={'ourServiceImage'} className={'mx-auto'} />
             <div className={'md:text-[16px] text-[12px]'}>
                 <p className={'whitespace-nowrap mb-2 font-extrabold text-xl text-[#085B5D]'}>{text1}</p>
                 <p className={'text-white'}>{text2}</p>
-                <p className={'text-white mb-2'}>{text3}</p>
+                <p onClick={onClick} className={'text-white mb-2 '}>{text3}</p>
             </div>
-            <p className={'whitespace-nowrap text-[15px] md:text-[20px] rounded-3xl px-4 md:px-6 py-2 justify-center items-center text-white bg-[#085B5D]'}>{text4}</p>
+            <p className={'hover:bg-[#D1D1D6] hover:cursor hover:shadow-xl  transition duration-300 ease-in-out whitespace-nowrap text-[15px] md:text-[20px] rounded-3xl px-4 md:px-6 py-2 justify-center items-center text-white bg-[#085B5D]'}>{text4}</p>
         </div>
     );
 }
--- bondex/types/Types.d.ts
+++ bondex/types/Types.d.ts
@@ -1,11 +1,11 @@
 
 export type OurServiceProps ={
     image: string | StaticImageData;
-    text1?: string,
-    text2?: string,
-    text3?: string,
-    text4?: string,
-
+    text1?: string;
+    text2?: string;
+    text3?: string;
+    text4?: string;
+    onClick?: ()=> void;
 }
 
 export type OverlayProps={
