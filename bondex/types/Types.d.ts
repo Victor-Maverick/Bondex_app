c561abd75b83318bd61bbe0393835f5ae6ebbe80
Yisaterese <teresejosephyisa@gmail.com>
2024-11-29 01:29:06 +0100

    finished styling the signup modal

--- bondex/types/Types.d.ts
+++ bondex/types/Types.d.ts
@@ -1,11 +1,15 @@
 
-export type OurServiceProps ={
+export type ServiceProps ={
     image: string | StaticImageData;
     text1?: string;
     text2?: string;
     text3?: string;
     text4?: string;
     onClick?: ()=> void;
+}
+
+export type SignUpModalProps = {
+    onClose: () => void;
 }
 
 export type OverlayProps={
